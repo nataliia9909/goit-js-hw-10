145504b4c56252e72b021a5adb0fe76ba2728b30
nataliia9909 <113458747+nataliia9909@users.noreply.github.com>
2023-03-20 22:11:30 +0300

    added corections of mentor

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,6 +1,9 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 export function fetchCountries(name) {
-    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`).then(response => { 
+    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`).then(response => {
+        if (!response.ok) {
+          throw new Error(response.status)  
+        } 
     return response.json()})
 }
 
